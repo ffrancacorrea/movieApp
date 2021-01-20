9944593305e74ddb6e55ce96680e2ae6d20c1000
ffrancacorrea <>
2021-01-20 16:36:25 +0100

    added the base style to the cards

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <div className="header">
-      <h1 className="title">React Movie Search</h1>
+      <h1 className="main_title">Movie Search</h1>
     </div>
   )
 }
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -5,6 +5,8 @@ export default function Search(props) {
     const inputRef = useRef();
     const [query, setQuery] = useState('');
     const [movies, setMovies] = useState([]);
+    const poster_w185 = "https://image.tmdb.org/t/p/w185/"
+    //const poster_w500 = "https://image.tmdb.org/t/p/w500/"
     
     useEffect(() => {
         inputRef.current.focus();
@@ -13,10 +15,11 @@ export default function Search(props) {
     const searchMovies = async (e) => {
         e.preventDefault();
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&page=1&include_adult=false`;
-
+        
         try {
             const res = await fetch(url);
             const data  = await res.json();
+            console.log(data)
             setMovies(data.results);
         }catch(err){
             console.error(err);
@@ -36,8 +39,12 @@ export default function Search(props) {
                 {movies.map(movie => (
                     <div className="card" key={movie.id}>
                         <img className="card__image"
-                            src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}
-                            />
+                            src={`${poster_w185}${movie.poster_path}`}
+                            alt="poster"/>
+                            
+                        <div className="card__description">
+                            <h2>{movie.original_title}</h2>
+                        </div>
                     </div>
                 ))}
             </div>    
